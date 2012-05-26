e73f750be5a0481ec3f4c22c7d6aedb43ae0eba4
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2012-05-25 22:12:48 -0300

    Minor organizations on explodeMultiStr().

--- src/common/util_str.c
+++ src/common/util_str.c
@@ -61,30 +61,26 @@ void explodeMultiStr(const wchar_t *multiStr, Strings *pBuf)
 	// Will be splitted into an array of pointer to strings.
 	// Assumes a well-formed multiStr.
 
-	int numStrings = 0;
+	int i, numStrings = 0;
+	const wchar_t *pRun = NULL;
 
-	// Count number of null-delimited strings; string end with double null.
-	{
-		const wchar_t *pRun = multiStr;
-		while(*pRun) {
-			++numStrings;
-			pRun += lstrlen(pRun) + 1;
-		}
+	// Count number of null-delimited strings; string end with double null.	
+	pRun = multiStr;
+	while(*pRun) {
+		++numStrings;
+		pRun += lstrlen(pRun) + 1;
 	}
 
 	// Alloc array of pointers to arrays (strings).
 	Strings_realloc(pBuf, numStrings);
 
-	// Alloc and copy each string.
-	{
-		int i;
-		const wchar_t *pRun = multiStr;
-		for(i = 0; i < numStrings; ++i) {
-			int len = lstrlen(pRun);
-			Strings_get(pBuf, i) = malloc(sizeof(wchar_t) * (len + 1));
-			memcpy(Strings_get(pBuf, i), pRun, sizeof(wchar_t) * (len + 1));
-			pRun += len + 1;
-		}
+	// Alloc and copy each string.	
+	pRun = multiStr;
+	for(i = 0; i < numStrings; ++i) {
+		int len = lstrlen(pRun);
+		Strings_get(pBuf, i) = malloc(sizeof(wchar_t) * (len + 1));
+		memcpy(Strings_get(pBuf, i), pRun, sizeof(wchar_t) * (len + 1));
+		pRun += len + 1;
 	}
 }
 
