20b6612302ce9add87531016cd1659c2e1cf27eb
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-09-11 10:34:24 -0300

    Parallel tag parsing with channels and goroutines.

--- DlgMainFuncs.go
+++ DlgMainFuncs.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"id3fit/id3"
 	"id3fit/prompt"
-	"sync"
 
 	"github.com/rodrigocfd/windigo/win"
 )
@@ -16,39 +15,40 @@ func (me *DlgMain) addFilesToList(mp3s []string) {
 		Tag *id3.Tag
 	}
 
-	wait := sync.WaitGroup{}
-	mutex := sync.Mutex{}
-	results := make([]Result, 0, len(mp3s))
+	go func() {
+		resultChan := make(chan Result, len(mp3s))
+		results := make([]Result, 0, len(mp3s))
 
-	for _, mp3 := range mp3s {
-		wait.Add(1)
-		go func(mp3 string) {
-			defer wait.Done()
-			tag, err := id3.ReadTagFromFile(mp3)
-			mutex.Lock()
-			results = append(results, Result{
-				Mp3: mp3,
-				Err: err,
-				Tag: tag,
-			})
-			mutex.Unlock()
-		}(mp3)
-	}
-	wait.Wait()
-
-	for _, resu := range results {
-		if resu.Err != nil {
-			prompt.Error(me.wnd, "Error parsing tag", "",
-				fmt.Sprintf("File:\n%s\n\n%s", resu.Mp3, resu.Err))
-		} else {
-			if _, found := me.lstFiles.Items().Find(resu.Mp3); !found { // file not added yet?
-				me.lstFiles.Items().
-					AddWithIcon(0, resu.Mp3, fmt.Sprintf("%d", resu.Tag.OriginalPadding())) // will fire LVN_INSERTITEM
-			}
-			me.cachedTags[resu.Mp3] = resu.Tag // cache (or re-cache) the tag
+		for _, mp3 := range mp3s {
+			go func(mp3 string) {
+				tag, err := id3.ReadTagFromFile(mp3)
+				resultChan <- Result{ // send all results in parallel
+					Mp3: mp3,
+					Err: err,
+					Tag: tag,
+				}
+			}(mp3)
 		}
-	}
-	me.lstFiles.Columns().SetWidthToFill(0)
+		for i := 0; i < len(mp3s); i++ {
+			results = append(results, <-resultChan) // receive all results
+		}
+
+		me.wnd.RunUiThread(func() {
+			for _, resu := range results { // analyze all results
+				if resu.Err != nil {
+					prompt.Error(me.wnd, "Error parsing tag", "",
+						fmt.Sprintf("File:\n%s\n\n%s", resu.Mp3, resu.Err))
+				} else {
+					if _, found := me.lstFiles.Items().Find(resu.Mp3); !found { // file not added yet?
+						me.lstFiles.Items().
+							AddWithIcon(0, resu.Mp3, fmt.Sprintf("%d", resu.Tag.OriginalPadding())) // will fire LVN_INSERTITEM
+					}
+					me.cachedTags[resu.Mp3] = resu.Tag // cache (or re-cache) the tag
+				}
+			}
+			me.lstFiles.Columns().SetWidthToFill(0)
+		})
+	}()
 }
 
 func (me *DlgMain) displayTagsOfSelectedFiles() {
