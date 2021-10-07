52ae428c1e0ad929400376937ec8483095174515
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-10-07 16:17:21 -0300

    Create constants with iota.

--- ids.go
+++ ids.go
@@ -1,22 +1,25 @@
 package main
 
-const APP_TITLE = "ID3 Fit"
+const (
+	APP_TITLE = "ID3 Fit"
+	ICO_MAIN  = 101
+)
 
 const (
-	ICO_MAIN int = 101
+	LST_FILES = iota + 1001
+	LST_FRAMES
+)
 
-	LST_FILES  int = 1001
-	LST_FRAMES int = 1002
-
-	MNU_MAIN        int = 200
-	MNU_OPEN        int = 201
-	MNU_DELETE      int = 202
-	MNU_REM_PAD     int = 203
-	MNU_REM_RG      int = 204
-	MNU_REM_RG_PIC  int = 205
-	MNU_PREFIX_YEAR int = 206
-	MNU_CLEAR_DIACR int = 207
-	MNU_ABOUT       int = 208
+const (
+	MNU_MAIN = iota + 200
+	MNU_OPEN
+	MNU_DELETE
+	MNU_REM_PAD
+	MNU_REM_RG
+	MNU_REM_RG_PIC
+	MNU_PREFIX_YEAR
+	MNU_CLEAR_DIACR
+	MNU_ABOUT
 )
 
 const (
