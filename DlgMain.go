1e0f695a133ae177786d5ed50d5e2fb1b71a9bd1
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-10-10 11:23:47 -0300

    Tag parsing/serializing without goroutines.

--- DlgMain.go
+++ DlgMain.go
@@ -77,52 +77,94 @@ func (me *DlgMain) Run() int {
 	return me.wnd.RunAsMain()
 }
 
+// func (me *DlgMain) addFilesToList(mp3s []string, onFinish func()) {
+// 	go func() {
+// 		type OutputData struct {
+// 			Mp3 string
+// 			Err error
+// 			Tag *id3.Tag
+// 		}
+
+// 		// Parse all tags from files in parallel.
+
+// 		processChan := make(chan OutputData, len(mp3s))
+// 		outputUnits := make([]OutputData, 0, len(mp3s)) // will receive processing results
+
+// 		for _, mp3 := range mp3s {
+// 			go func(mp3 string) {
+// 				tag, err := id3.ReadTagFromFile(mp3)
+// 				processChan <- OutputData{ // send all results to channel
+// 					Mp3: mp3,
+// 					Err: err,
+// 					Tag: tag,
+// 				}
+// 			}(mp3)
+// 		}
+// 		for i := 0; i < len(mp3s); i++ {
+// 			outputUnits = append(outputUnits, <-processChan) // receive all results from channel
+// 		}
+
+// 		// Back to UI thread, display results.
+
+// 		me.wnd.RunUiThread(func() {
+// 			for _, resu := range outputUnits {
+// 				if resu.Err != nil {
+// 					prompt.Error(me.wnd, "Error parsing tag", nil,
+// 						fmt.Sprintf("File:\n%s\n\n%s", resu.Mp3, resu.Err))
+// 				} else {
+// 					if item, found := me.lstFiles.Items().Find(resu.Mp3); !found { // file not added yet?
+// 						me.lstFiles.Items().
+// 							AddWithIcon(0, resu.Mp3, strconv.Itoa(resu.Tag.OriginalPadding())) // will fire LVN_INSERTITEM
+// 					} else {
+// 						item.SetText(1, strconv.Itoa(resu.Tag.OriginalPadding())) // update padding info
+// 					}
+// 					me.cachedTags[resu.Mp3] = resu.Tag // cache (or re-cache) the tag
+// 				}
+// 			}
+// 			me.lstFiles.Columns().SetWidthToFill(0)
+// 			me.displayFramesOfSelectedFiles()
+// 			if onFinish != nil {
+// 				onFinish()
+// 			}
+// 		})
+// 	}()
+// }
+
 func (me *DlgMain) addFilesToList(mp3s []string, onFinish func()) {
-	go func() {
-		type OutputData struct {
-			Mp3 string
-			Err error
-			Tag *id3.Tag
-		}
-
-		// Parse all tags from files in parallel.
-
-		processChan := make(chan OutputData, len(mp3s))
-		outputUnits := make([]OutputData, 0, len(mp3s)) // will receive processing results
+	go func() { // launch a separated thread
+		halted := false
 
 		for _, mp3 := range mp3s {
-			go func(mp3 string) {
-				tag, err := id3.ReadTagFromFile(mp3)
-				processChan <- OutputData{ // send all results to channel
-					Mp3: mp3,
-					Err: err,
-					Tag: tag,
-				}
-			}(mp3)
-		}
-		for i := 0; i < len(mp3s); i++ {
-			outputUnits = append(outputUnits, <-processChan) // receive all results from channel
+			tag, err := id3.ReadTagFromFile(mp3) // read all files sequentially
+			if err != nil {
+				me.wnd.RunUiThread(func() {
+					prompt.Error(me.wnd, "Error parsing tag", nil,
+						fmt.Sprintf("File:\n%s\n\n%s", mp3, err))
+				})
+				halted = true // nothing else will be done
+				break
+			}
+			me.cachedTags[mp3] = tag // cache (or re-cache) tag
 		}
 
-		// Back to UI thread, display results.
+		if halted {
+			return
+		}
 
 		me.wnd.RunUiThread(func() {
-			for _, resu := range outputUnits {
-				if resu.Err != nil {
-					prompt.Error(me.wnd, "Error parsing tag", nil,
-						fmt.Sprintf("File:\n%s\n\n%s", resu.Mp3, resu.Err))
+			me.lstFiles.SetRedraw(false)
+			for _, mp3 := range mp3s {
+				tag := me.cachedTags[mp3]
+				if item, found := me.lstFiles.Items().Find(mp3); !found { // file not added yet?
+					me.lstFiles.Items().
+						AddWithIcon(0, mp3, strconv.Itoa(tag.OriginalPadding()))
 				} else {
-					if item, found := me.lstFiles.Items().Find(resu.Mp3); !found { // file not added yet?
-						me.lstFiles.Items().
-							AddWithIcon(0, resu.Mp3, strconv.Itoa(resu.Tag.OriginalPadding())) // will fire LVN_INSERTITEM
-					} else {
-						item.SetText(1, strconv.Itoa(resu.Tag.OriginalPadding())) // update padding info
-					}
-					me.cachedTags[resu.Mp3] = resu.Tag // cache (or re-cache) the tag
+					item.SetText(1, strconv.Itoa(tag.OriginalPadding())) // update padding
 				}
 			}
+			me.lstFiles.SetRedraw(true)
 			me.lstFiles.Columns().SetWidthToFill(0)
-			me.displayFramesOfSelectedFiles()
+
 			if onFinish != nil {
 				onFinish()
 			}
@@ -179,56 +221,83 @@ func (me *DlgMain) displayFramesOfSelectedFiles() {
 	me.lstValues.Hwnd().EnableWindow(len(selItems) > 0) // if no files selected, disable lstValues
 }
 
+// func (me *DlgMain) reSaveTagsOfSelectedFiles(onFinish func()) {
+// 	type InputData struct {
+// 		Mp3 string
+// 		Tag *id3.Tag
+// 	}
+// 	type OutputData struct {
+// 		Mp3 string
+// 		Err error
+// 	}
+
+// 	inputUnits := make([]InputData, 0, me.lstFiles.Items().SelectedCount())
+// 	for _, selItem := range me.lstFiles.Items().Selected() {
+// 		selMp3 := selItem.Text(0)
+// 		inputUnits = append(inputUnits, InputData{ // prepare data to be worked upon
+// 			Mp3: selMp3,
+// 			Tag: me.cachedTags[selMp3],
+// 		})
+// 	}
+
+// 	go func() {
+// 		processChan := make(chan OutputData, len(inputUnits))
+// 		outputUnits := make([]OutputData, 0, len(inputUnits)) // will receive processing results
+
+// 		for i := range inputUnits {
+// 			go func(i int) {
+// 				selUnit := inputUnits[i]
+// 				err := selUnit.Tag.SerializeToFile(selUnit.Mp3)
+// 				processChan <- OutputData{ // send all results in parallel
+// 					Mp3: selUnit.Mp3,
+// 					Err: err,
+// 				}
+// 			}(i)
+// 		}
+// 		for i := 0; i < len(inputUnits); i++ {
+// 			outputUnits = append(outputUnits, <-processChan) // receive all results
+// 		}
+
+// 		me.wnd.RunUiThread(func() {
+// 			reCachedMp3s := make([]string, 0, len(outputUnits))
+
+// 			for _, outputUnit := range outputUnits { // analyze all results
+// 				if outputUnit.Err != nil {
+// 					prompt.Error(me.wnd, "Writing error", nil,
+// 						fmt.Sprintf("Failed to write tag to:\n%s\n\n%s",
+// 							outputUnit.Mp3, outputUnit.Err.Error()))
+// 				} else {
+// 					reCachedMp3s = append(reCachedMp3s, outputUnit.Mp3)
+// 				}
+// 			}
+// 			me.addFilesToList(reCachedMp3s, onFinish)
+// 		})
+// 	}()
+// }
+
 func (me *DlgMain) reSaveTagsOfSelectedFiles(onFinish func()) {
-	type InputData struct {
-		Mp3 string
-		Tag *id3.Tag
-	}
-	type OutputData struct {
-		Mp3 string
-		Err error
-	}
+	go func() { // launch a separated thread
+		halted := false
+		selMp3s := make([]string, 0, me.lstFiles.Items().SelectedCount())
 
-	inputUnits := make([]InputData, 0, me.lstFiles.Items().SelectedCount())
-	for _, selItem := range me.lstFiles.Items().Selected() {
-		selMp3 := selItem.Text(0)
-		inputUnits = append(inputUnits, InputData{ // prepare data to be worked upon
-			Mp3: selMp3,
-			Tag: me.cachedTags[selMp3],
-		})
-	}
-
-	go func() {
-		processChan := make(chan OutputData, len(inputUnits))
-		outputUnits := make([]OutputData, 0, len(inputUnits)) // will receive processing results
-
-		for i := range inputUnits {
-			go func(i int) {
-				selUnit := inputUnits[i]
-				err := selUnit.Tag.SerializeToFile(selUnit.Mp3)
-				processChan <- OutputData{ // send all results in parallel
-					Mp3: selUnit.Mp3,
-					Err: err,
-				}
-			}(i)
+		for _, selItem := range me.lstFiles.Items().Selected() {
+			mp3 := selItem.Text(0)
+			selMp3s = append(selMp3s, mp3)
+			tag := me.cachedTags[mp3]
+			if err := tag.SerializeToFile(mp3); err != nil {
+				prompt.Error(me.wnd, "Writing error", nil,
+					fmt.Sprintf("Failed to write tag to:\n%sn\n\n%s", mp3, err.Error()))
+				halted = true // nothing else will be done
+				break
+			}
 		}
-		for i := 0; i < len(inputUnits); i++ {
-			outputUnits = append(outputUnits, <-processChan) // receive all results
+
+		if halted {
+			return
 		}
 
 		me.wnd.RunUiThread(func() {
-			reCachedMp3s := make([]string, 0, len(outputUnits))
-
-			for _, outputUnit := range outputUnits { // analyze all results
-				if outputUnit.Err != nil {
-					prompt.Error(me.wnd, "Writing error", nil,
-						fmt.Sprintf("Failed to write tag to:\n%s\n\n%s",
-							outputUnit.Mp3, outputUnit.Err.Error()))
-				} else {
-					reCachedMp3s = append(reCachedMp3s, outputUnit.Mp3)
-				}
-			}
-			me.addFilesToList(reCachedMp3s, onFinish)
+			me.addFilesToList(selMp3s, onFinish)
 		})
 	}()
 }
