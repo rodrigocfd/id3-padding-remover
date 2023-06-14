6112ed39e1412ebc8f71418482d53d99afc6281a
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-06-14 08:42:19 -0300

    Renaming deployment script.

--- res/deploy.sh
+++ res/deploy.sh
@@ -1,11 +1,11 @@
-# Builds the final application.
+# Builds the final application and copies to our local dir.
 # Run inside res/ directory.
 
 echo "Building application..."
 cd ..
 go build -ldflags "-s -w -H=windowsgui"
 
-echo "Moving to apps folder..."
+echo "Deploying to apps folder..."
 mv ./id3fit.exe /d/Stuff/apps/_audio\ tools/.
 
 echo "Done."
