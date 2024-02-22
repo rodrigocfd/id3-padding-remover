f20492e0443c68280c72ff098b93e73f0f23f0a5
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-02-21 21:55:30 -0300

    Remove selected files on DEL key.

--- src/wnd_main/wnd_main_list.rs
+++ src/wnd_main/wnd_main_list.rs
@@ -1,4 +1,4 @@
-use winsafe::{self as w, prelude::*};
+use winsafe::{self as w, prelude::*, co};
 
 use super::WndMain;
 
@@ -7,6 +7,14 @@ impl WndMain {
 		let self2 = self.clone();
 		self.lst_files.on().lvn_item_changed(move |_| {
 			self2.update_num_files();
+			Ok(())
+		});
+
+		let self2 = self.clone();
+		self.lst_files.on().lvn_key_down(move |p| {
+			if p.wVKey == co::VK::DELETE { // on DEL key, remove selected files from the list
+				self2.lst_files.items().delete_selected();
+			}
 			Ok(())
 		});
 	}
