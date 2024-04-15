2a05e19b79b8ba204c7edc87df2b098fbcb09761
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-04-15 11:23:10 -0300

    Larger picture window.

--- src/wnd_edit/wnd_edit_ctor.rs
+++ src/wnd_edit/wnd_edit_ctor.rs
@@ -54,7 +54,7 @@ impl WndEdit {
 			FieldPack::new_edit(id3v2::Field::OrigAlbum, &wnd, ids::CHK_ORIG_ALBUM),
 			FieldPack::new_edit(id3v2::Field::OrigYear, &wnd, ids::CHK_ORIG_YEAR),
 		]));
-		let wnd_pic = WndPicture::new(&wnd, sel_tags.clone(), (264, 22), (90, 90), NN)?;
+		let wnd_pic = WndPicture::new(&wnd, sel_tags.clone(), (255, 22), (100, 100), NN)?;
 		let lst_frames = gui::ListView::new_dlg(&wnd, ids::LST_FRAMES, NN, None);
 
 		let new_self = Self { wnd, btn_ok, btn_cancel, field_packs, wnd_pic, sel_tags, lst_frames };
