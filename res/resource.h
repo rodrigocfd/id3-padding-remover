ba78bf160ba31ac3f3bda3baa570359fd7e65df7
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-08-09 09:55:40 -0300

    Edit dialog resource.

--- res/resource.h
+++ res/resource.h
@@ -8,6 +8,38 @@
 #define MNU_FILE                        104
 #define ACC_MAIN                        105
 #define LST_FILES                       1001
+#define CHK_ARTIST                      1002
+#define TXT_ARTIST                      1003
+#define CHK_TITLE                       1004
+#define TXT_TITLE                       1005
+#define CHK_SUBTITLE                    1006
+#define TXT_SUBTITLE                    1007
+#define CHK_ALBUM                       1008
+#define TXT_ALBUM                       1009
+#define CHK_TRACK                       1010
+#define TXT_TRACK                       1011
+#define CHK_YEAR                        1012
+#define TXT_YEAR                        1013
+#define CHK_GENRE                       1014
+#define CMB_GENRE                       1015
+#define CHK_PERFORMER                   1016
+#define TXT_PERFORMER                   1017
+#define CHK_PUBLISHER                   1018
+#define TXT_PUBLISHER                   1019
+#define CHK_OARTIST                     1020
+#define TXT_OARTIST                     1021
+#define CHK_OALBUM                      1022
+#define TXT_OALBUM                      1023
+#define CHK_OYEAR                       1024
+#define TXT_OYEAR                       1025
+#define CHK_COMPOSER                    1026
+#define TXT_COMPOSER                    1027
+#define CHK_LYRICIST                    1028
+#define TXT_LYRICIST                    1029
+#define CHK_COMMENT                     1030
+#define TXT_COMMENT                     1031
+#define BTN_UNCHECK                     1032
+#define LST_FRAMES                      1033
 #define MNU_FILE_OPEN                   40001
 #define MNU_FILE_EDIT                   40002
 #define MNU_FILE_REMOVE                 40003
@@ -19,7 +51,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40005
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
