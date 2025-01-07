fa14d60b4654acb04bd8eff6577fedf844abe09c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-01-07 14:22:47 -0300

    New erase_and_write API.

--- src/id3v2/tag.rs
+++ src/id3v2/tag.rs
@@ -145,13 +145,12 @@ impl Tag {
 		let current_contents = fout.read_all()?; // read the whole MP3 into a buffer
 		let current_tag = Self::parse(&current_contents)?; // parse tag currently saved in the MP3 file
 
-		fout.set_size(0)?; // truncate file
 		if self.frames.is_empty() {
-			fout.write(
+			fout.erase_and_write(
 				&current_contents[current_tag.mp3_offset as _..], // no tag will be written
 			)?;
 		} else {
-			fout.write(
+			fout.erase_and_write(
 				&self.serialize().into_iter()
 					.chain(current_contents[current_tag.mp3_offset as _..].iter().map(|b| *b))
 					.collect::<Vec<_>>(),
