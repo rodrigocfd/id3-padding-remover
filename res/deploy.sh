eade3ec637452300277ab0ed637fc615fc893024
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-08-17 14:36:36 -0300

    Improve build script.

--- res/deploy.sh
+++ res/deploy.sh
@@ -5,7 +5,8 @@ echo "Building application..."
 cd ..
 go build -ldflags "-s -w -H=windowsgui"
 
+EXE=id3-fit_GO.exe
 echo "Deploying to apps folder..."
-mv ./id3fit.exe /d/Stuff/apps/_audio\ tools/id3-fit_GO.exe
+mv ./id3fit.exe /d/Stuff/apps/_audio\ tools/$EXE
 
-/c/apps-portable/Go/gsa /d/Stuff/apps/_audio\ tools/id3-fit_GO.exe
+/c/apps-portable/Go/gsa /d/Stuff/apps/_audio\ tools/$EXE
